e57c9e9e3466ae9ee78d3dd41d245af195ac0712
Uno Coder <unocoder1@gmail.com>
2022-10-22 22:43:42 +0200

    remove non-ascii characters

--- chrome/scripts/text-steganography.js
+++ chrome/scripts/text-steganography.js
@@ -1,4 +1,9 @@
 function getConcealedText(config, originalText) {
 	if (config.debug === true) return "debug";
-	return originalText;
+
+	var concealedText = originalText.slice();
+
+	var concealedText = originalText.replace(/[\u{0080}-\u{FFFF}]/gu,"");
+
+	return concealedText;
 }
