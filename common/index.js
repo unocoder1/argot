50289e07e106d68318761e6ed009007269df4b8b
unocoder1 <unocoder1@gmail.com>
2022-10-22 23:22:38 +0200

    add index.js and a stub for the first transformation

---
+++ common/index.js
@@ -0,0 +1,9 @@
+function getConcealedText(config, originalText) {
+	if (config.debug === true) return "debug";
+
+	var concealedText = originalText.slice();
+
+	var concealedText = originalText.replace(/[\u{0080}-\u{FFFF}]/gu,"");
+
+	return concealedText;
+}
---
+++ common/transformations/remove-non-ascii.js
@@ -0,0 +1,11 @@
+export const RemoveNonASCII = {
+
+    encode: (_config, originalText) => {
+
+    },
+
+    decode: (_config, originalText) => {
+        return originalText;
+    }
+
+};
