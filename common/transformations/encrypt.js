a10580317b4d0b8ba8daf93adaca87a9d53ad8ab
unocoder1 <unocoder1@gmail.com>
2022-10-25 00:36:16 +0200

    use base64 instead of hex at the end of encryption

--- common/transformations/encrypt.js
+++ common/transformations/encrypt.js
@@ -7,12 +7,12 @@ module.exports = {
 
     getEncoded: (config, text) => {
         const cipher = crypto.createCipher(algorithm, config.password);
-        return cipher.update(text, 'utf8', 'hex') + cipher.final('hex');
+        return cipher.update(text, 'utf8', 'base64') + cipher.final('base64');
     },
 
     getDecoded: (config, text) => {
         const decipher = crypto.createDecipher(algorithm, config.password);
-        return decipher.update(text, 'hex', 'utf8') + decipher.final('utf8');
+        return decipher.update(text, 'base64', 'utf8') + decipher.final('utf8');
     }
 
 };
