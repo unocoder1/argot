a7bfa9da2024f179411e70f07f297591a416a5c5
Uno Coder <unocoder1@gmail.com>
2022-10-27 17:55:02 +0200

    add selectors for old reddit text editor and submit button

--- chrome/scripts/content.js
+++ chrome/scripts/content.js
@@ -1,6 +1,11 @@
-const textsToHide = document.querySelectorAll(":root > body > div.content div.usertext-body > div.md");
-
+const textsToHide = document.querySelectorAll(':root > body > div.content div.usertext-body > div.md');
 console.log(textsToHide);
+
+const editorArea = document.querySelector(':root > body > div.content > div.commentarea > form > div.usertext-edit > div.md > textarea');
+console.log(editorArea);
+
+const editorOriginalSubmitButton = document.querySelector(':root > body > div.content > div.commentarea > form > div.usertext-edit > div.bottom-area > div.usertext-buttons > button[type="submit"]');
+console.log(editorOriginalSubmitButton);
 
 textsToHide.forEach((currentValue, currentIndex, listObj) => {
 	currentValue.innerText = getConcealedText(defaultConfig, currentValue.innerText);
