0797e3d744efdcf4428c3443c65529210279e500
Uno Coder <unocoder1@gmail.com>
2022-11-13 14:48:26 +0100

    reference restore defaults button in popup.js

--- chrome/popup.js
+++ chrome/popup.js
@@ -1,11 +1,6 @@
-const encodeSelectedTextButton = document.getElementById("encodeSelectedTextButton");
-encodeSelectedTextButton.addEventListener("click", () => {
-	chrome.tabs.query ({active: true, currentWindow: true}, function(tabs) {
-		chrome.tabs.sendMessage(tabs[0].id, {body: "getSelection"}, function(message) {
-			console.log("aaaaand back");
-		});
-	});
-	/*const text = getSelectedText();
-	const encodedText = TextSteganography.getEncoded(text);
-	replaceFocusedText(encodedText);*/
+console.log("popup.js loaded");
+
+const restoreDefaultsButton = document.getElementById("restore_defaults");
+restoreDefaultsButton.addEventListener("click", () => {
+	console.log("clicked");
 });
