e0b04e49981a30e71e8d8b4f709dabb74530a23e
Uno Coder <unocoder1@gmail.com>
2022-11-16 00:00:14 +0100

    update createTransformation to properly handle new config

--- common/transformations/index.js
+++ common/transformations/index.js
@@ -8,13 +8,13 @@ module.exports.MapThroughDictionary = require('./map-through-dictionary');
 
 module.exports.createTransformation = (config) => {
     var transformations = [];
-    if (config.removeNonASCII === true) { transformations.push(module.exports.RemoveNonASCII); }
-    if (config.compressPlainText === true) { transformations.push(module.exports.Compress); }
+    if (config.removeNonASCII === 1) { transformations.push(module.exports.RemoveNonASCII); }
+    if (config.compressPlainText === 1) { transformations.push(module.exports.Compress); }
     // TODO: Assert if password is provided.
-    if (config.encrypt === true) { transformations.push(module.exports.Encrypt); }
-    if (config.normalizeFrequencies === true) { transformations.push(module.exports.NormalizeFrequencies); }
-    if (config.generateFakeText === true) { transformations.push(module.exports.GenerateFakeText2); }
-    if (config.mapThroughDictionary === true) { transformations.push(module.exports.MapThroughDictionary); }
+    if (config.encrypt === "A") { transformations.push(module.exports.Encrypt); }
+    if (config.normalizeFrequencies === 1) { transformations.push(module.exports.NormalizeFrequencies); }
+    if (config.generateFakeText === "A") { transformations.push(module.exports.GenerateFakeText2); }
+    if (config.mapThroughDictionary === 1) { transformations.push(module.exports.MapThroughDictionary); }
 
     return {
         getEncoded: (config, text) => {
