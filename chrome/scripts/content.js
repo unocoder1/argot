7c299483060c16ede75291076b86d446f331d167
Uno Coder <unocoder1@gmail.com>
2022-11-17 00:10:10 +0100

    query current config in content.js

--- chrome/scripts/content.js
+++ chrome/scripts/content.js
@@ -23,35 +23,38 @@ chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
     }
 });
 
-const textsToHide = document.querySelectorAll(':root > body > div.content div.usertext-body > div.md');
-console.log(textsToHide);
 
-const editorArea = document.querySelector(':root > body > div.content > div.commentarea > form > div.usertext-edit > div.md > textarea');
-console.log(editorArea);
+chrome.storage.sync.get('currentConfig', (result) => {
+    const currentConfig = result.currentConfig;
 
-const editorOriginalSubmitButton = document.querySelector(':root > body > div.content > div.commentarea > form > div.usertext-edit > div.bottom-area > div.usertext-buttons > button[type="submit"]');
-console.log(editorOriginalSubmitButton);
-const editorEncodeButton = editorOriginalSubmitButton.cloneNode(false);
-editorEncodeButton.id += 0; // TODO: Ensure unique ID.
-editorEncodeButton.textContent  = "Encode";
-editorEncodeButton.onclick = function(){
-    editorArea.value = TextSteganography.getEncodedText(defaultConfig, editorArea.value);
-    return false;  // Return false to stop form submit.
-};
-editorOriginalSubmitButton.parentNode.appendChild(editorEncodeButton);
-const editorNewSubmitButton = editorOriginalSubmitButton.cloneNode(false);
-editorNewSubmitButton.id += 1; // TODO: Ensure unique ID.
-editorNewSubmitButton.onclick = function(){
-    editorArea.value = TextSteganography.getEncodedText(defaultConfig, editorArea.value);
-    return true;  // Return true to NOT stop form submit.
-};
-editorNewSubmitButton.textContent  = "Encode and Submit";
-editorOriginalSubmitButton.parentNode.appendChild(editorNewSubmitButton);
+    const textsToHide = document.querySelectorAll(':root > body > div.content div.usertext-body > div.md');
 
-textsToHide.forEach((currentValue, currentIndex, listObj) => {
-    try {
-        currentValue.innerText = TextSteganography.getDecodedText(defaultConfig, currentValue.innerText);
-    } catch (error) {
-        console.log(error);
-    }
+    const editorArea = document.querySelector(':root > body > div.content > div.commentarea > form > div.usertext-edit > div.md > textarea');
+
+    const editorOriginalSubmitButton = document.querySelector(':root > body > div.content > div.commentarea > form > div.usertext-edit > div.bottom-area > div.usertext-buttons > button[type="submit"]');
+    const editorEncodeButton = editorOriginalSubmitButton.cloneNode(false);
+    editorEncodeButton.id += 0; // TODO: Ensure unique ID.
+    editorEncodeButton.textContent  = "Encode";
+    editorEncodeButton.onclick = function(){
+        editorArea.value = TextSteganography.getEncodedText(currentConfig, editorArea.value);
+        return false;  // Return false to stop form submit.
+    };
+    editorOriginalSubmitButton.parentNode.appendChild(editorEncodeButton);
+    const editorNewSubmitButton = editorOriginalSubmitButton.cloneNode(false);
+    editorNewSubmitButton.id += 1; // TODO: Ensure unique ID.
+    editorNewSubmitButton.onclick = function(){
+        editorArea.value = TextSteganography.getEncodedText(currentConfig, editorArea.value);
+        return true;  // Return true to NOT stop form submit.
+    };
+    editorNewSubmitButton.textContent  = "Encode and Submit";
+    editorOriginalSubmitButton.parentNode.appendChild(editorNewSubmitButton);
+
+    textsToHide.forEach((currentValue, currentIndex, listObj) => {
+        try {
+            currentValue.innerText = TextSteganography.getDecodedText(currentConfig, currentValue.innerText);
+        } catch (error) {
+            console.log(error);
+        }
+    });
+
 });
