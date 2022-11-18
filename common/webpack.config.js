bc71962abe480bf5115e6ec8ba9d2ac463f3b1c9
unocoder1 <unocoder1@gmail.com>
2022-11-18 23:27:56 +0100

    create bundle in bundle folder

--- common/webpack.config.js
+++ common/webpack.config.js
@@ -2,39 +2,39 @@ const webpack = require('webpack');
 const path = require('path');
 
 const config = {
-    mode: "production",
-    entry: './index.js',
-    output: {
-        path: __dirname,
-        filename: 'text-steganography-bundled.js',
-        library: {
-            name: "TextSteganography",
-            type: "var" 
-        },
+  mode: 'production',
+  entry: './index.js',
+  output: {
+    path: `${__dirname}/bundle`,
+    filename: 'text-steganography-bundled.js',
+    library: {
+      name: 'TextSteganography',
+      type: 'var',
     },
-    resolve: {
-        alias: {
-            "buffer": "buffer"
-        },
-        fallback: {
-            "assert": require.resolve("assert/"),
-            "buffer": require.resolve("buffer/"),
-            "crypto": require.resolve("crypto-browserify"),
-            "stream": require.resolve("stream-browserify"),
-            "zlib": require.resolve("browserify-zlib")
-        }
+  },
+  resolve: {
+    alias: {
+      buffer: 'buffer',
     },
-    optimization: {
-        minimize: false
+    fallback: {
+      assert: require.resolve('assert/'),
+      buffer: require.resolve('buffer/'),
+      crypto: require.resolve('crypto-browserify'),
+      stream: require.resolve('stream-browserify'),
+      zlib: require.resolve('browserify-zlib'),
     },
-    plugins: [
-        new webpack.DefinePlugin({
-          'process.env.NODE_DEBUG': "false",
-        }),
-        new webpack.ProvidePlugin({
-            Buffer: ['buffer', 'Buffer'],
-        }),
-    ],
+  },
+  optimization: {
+    minimize: false,
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env.NODE_DEBUG': 'false',
+    }),
+    new webpack.ProvidePlugin({
+      Buffer: ['buffer', 'Buffer'],
+    }),
+  ],
 };
 
 module.exports = config;
