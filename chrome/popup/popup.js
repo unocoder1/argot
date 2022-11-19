b70208c539cb70f67ad6ba77417f00222e099797
unocoder1 <unocoder1@gmail.com>
2022-11-19 01:36:05 +0100

    lint popup.js

--- chrome/popup/popup.js
+++ chrome/popup/popup.js
@@ -1,14 +1,15 @@
-console.log("popup.js loaded");
+/* global chrome */
+console.log('popup.js loaded');
 
-const restoreDefaultsButton = document.getElementById("restore_defaults");
+const restoreDefaultsButton = document.getElementById('restore_defaults');
 
-const passwordTextBox = document.getElementById("password");
-const removeNonASCIIFieldSet = document.getElementById("remove_non_ascii");
-const compressPlaintextFieldSet = document.getElementById("compress_plain_text");
-const encryptFieldSet = document.getElementById("encrypt");
-const normalizeFrequenciesFieldSet = document.getElementById("normalize_frequencies");
-const generateFakeTextFieldSet = document.getElementById("generate_fake_text");
-const mapThroughDictionaryFieldSet = document.getElementById("map_through_dictionary");
+const passwordTextBox = document.getElementById('password');
+const removeNonASCIIFieldSet = document.getElementById('remove_non_ascii');
+const compressPlaintextFieldSet = document.getElementById('compress_plain_text');
+const encryptFieldSet = document.getElementById('encrypt');
+const normalizeFrequenciesFieldSet = document.getElementById('normalize_frequencies');
+const generateFakeTextFieldSet = document.getElementById('generate_fake_text');
+const mapThroughDictionaryFieldSet = document.getElementById('map_through_dictionary');
 
 const configControls = [
     passwordTextBox,
@@ -17,25 +18,18 @@ const configControls = [
     encryptFieldSet,
     normalizeFrequenciesFieldSet,
     generateFakeTextFieldSet,
-    mapThroughDictionaryFieldSet
+    mapThroughDictionaryFieldSet,
 ];
-
-restoreDefaultsButton.addEventListener("click", () => {
-    getDefaultConfigFromStorage().then(config => {
-        setUI(config);
-        chrome.storage.sync.set({currentConfig: getConfigFromUI ()});
-    });
-});
 
 function getDefaultConfigFromStorage() {
     return new Promise((resolve, reject) => {
         chrome.storage.sync.get('defaultConfig', (result) => {
-          // Pass any observed errors down the promise chain.
-          if (chrome.runtime.lastError) {
-            return reject(chrome.runtime.lastError);
-          }
-          // Pass the data retrieved from storage down the promise chain.
-          resolve(result.defaultConfig);
+            // Pass any observed errors down the promise chain.
+            if (chrome.runtime.lastError) {
+                return reject(chrome.runtime.lastError);
+            }
+            // Pass the data retrieved from storage down the promise chain.
+            return resolve(result.defaultConfig);
         });
     });
 }
@@ -43,12 +37,12 @@ function getDefaultConfigFromStorage() {
 function getCurentConfigFromStorage() {
     return new Promise((resolve, reject) => {
         chrome.storage.sync.get('currentConfig', (result) => {
-          // Pass any observed errors down the promise chain.
-          if (chrome.runtime.lastError) {
-            return reject(chrome.runtime.lastError);
-          }
-          // Pass the data retrieved from storage down the promise chain.
-          resolve(result.currentConfig);
+            // Pass any observed errors down the promise chain.
+            if (chrome.runtime.lastError) {
+                return reject(chrome.runtime.lastError);
+            }
+            // Pass the data retrieved from storage down the promise chain.
+            return resolve(result.currentConfig);
         });
     });
 }
@@ -63,23 +57,8 @@ function setUI(config) {
     mapThroughDictionaryFieldSet.querySelector(`input[value="${config.mapThroughDictionary}"]`).checked = true;
 }
 
-
-function getConfigShorthandFromUI() {
-    const configShorthand = [0, 0, 0, 0, 0, 0];
-
-    configShorthand[0] = passwordTextBox.value;
-    configShorthand[1] = removeNonASCIIFieldSet.querySelector('input[type="radio"]:checked').value;
-    configShorthand[2] = compressPlaintextFieldSet.querySelector('input[type="radio"]:checked').value;
-    configShorthand[3] = encryptFieldSet.querySelector('input[type="radio"]:checked').value;
-    configShorthand[4] = normalizeFrequenciesFieldSet.querySelector('input[type="radio"]:checked').value;
-    configShorthand[5] = generateFakeTextFieldSet.querySelector('input[type="radio"]:checked').value;
-    configShorthand[6] = mapThroughDictionaryFieldSet.querySelector('input[type="radio"]:checked').value;
-
-    return configShorthand;
-}
-
 function getConfigFromUI() {
-    const config = { debug:false };
+    const config = { debug: false };
 
     config.password = passwordTextBox.value;
     config.removeNonASCII = removeNonASCIIFieldSet.querySelector('input[type="radio"]:checked').value;
@@ -92,29 +71,21 @@ function getConfigFromUI() {
     return config;
 }
 
-function getConfigShorthandFromDefaults() {
-    let configShorthand = [0, 0, 0, 0, 0, 0];
+restoreDefaultsButton.addEventListener('click', () => {
+    getDefaultConfigFromStorage().then((config) => {
+        setUI(config);
+        chrome.storage.sync.set({ currentConfig: getConfigFromUI() });
+    });
+});
 
-    configShorthand[0] = defaultConfig.password;
-    configShorthand[1] = defaultConfig.removeNonASCII;
-    configShorthand[2] = defaultConfig.compressPlainText;
-    configShorthand[3] = defaultConfig.encrypt;
-    configShorthand[4] = defaultConfig.normalizeFrequencies;
-    configShorthand[5] = defaultConfig.generateFakeText;
-    configShorthand[6] = defaultConfig.mapThroughDictionary;
-
-    console.log(configShorthand);
-    return configShorthand;
-}
-
-//chrome.storage.sync.get('defaultConfig', function(result) {
+// chrome.storage.sync.get('defaultConfig', function(result) {
 //    console.log('Value currently is ' + JSON.stringify(result.defaultConfig));
-//});
+// });
 
-getCurentConfigFromStorage().then(config => setUI(config));
+getCurentConfigFromStorage().then((config) => setUI(config));
 
-for (let i = 0; i < configControls.length; i++) {
-    configControls[i].addEventListener("change", function(e) {
-        chrome.storage.sync.set({currentConfig: getConfigFromUI ()});
+for (let i = 0; i < configControls.length; i += 1) {
+    configControls[i].addEventListener('change', (_e) => {
+        chrome.storage.sync.set({ currentConfig: getConfigFromUI() });
     });
 }
