2947e0027f03b2a153dc43ad388d2c6519b0b916
unocoder1 <unocoder1@gmail.com>
2022-11-19 01:35:37 +0100

    silence probably false lint error on Buffer.from on one line

--- common/transformations/compress.js
+++ common/transformations/compress.js
@@ -4,6 +4,7 @@ module.exports = {
 
     getEncoded: (_config, text) => zlib.deflateSync(text).toString('base64'),
 
+    // eslint-disable-next-line new-cap
     getDecoded: (_config, text) => zlib.inflateSync(new Buffer.from(text, 'base64')).toString(),
 
 };
